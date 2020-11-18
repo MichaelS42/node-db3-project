886c856698a2d13b74d7058df7bd95cd219d76b4
Michael Sanchez <michaelsanchez42@live.com>
2020-11-17 21:29:54 -0700

    might work

--- data/db-config.js
+++ data/db-config.js
@@ -1,1 +1,5 @@
 // db-config
+const knex = require('knex');
+const knexConfig = require('../knexfile');
+
+module.exports = knex(knexConfig.development);
--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,1 +1,58 @@
 // scheme-model
+const db = require("../data/dbConfig");
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  addStep,
+  update,
+};
+
+function find() {
+  try {
+    return db("schemes");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function findById(id) {
+  try {
+    return db("schems").where("id", id);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function findSteps(id) {
+  try {
+    return await db("steps").where("id", id);
+  } catch (error) {
+    console.error(error);
+  }
+}
+async function addStep(stepData, id) {
+  try {
+    await db("steps")
+      .inser({
+        scheme_id: id,
+        instructions: stepData,
+      })
+      .then((ids) => {
+        return findSteps(ids[0]);
+      });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function update(changes, id) {
+  await db("schemes")
+    .update(changes)
+    .where("id", id)
+    .then((id) => {
+      return findById(id);
+    });
+}
